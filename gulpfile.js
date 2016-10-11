597d8dbf2c48b71a1070b5540f889b8ab4baa60f
stubbornick <38818331+stubbornick@users.noreply.github.com>
2016-10-11 21:03:36 +0200

    * Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const gulp = require("gulp");
+const del = require("del");
+const babel = require("gulp-babel");
+const cache = require("gulp-cached");
+const sourcemaps = require("gulp-sourcemaps");
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
+const gutil = require("gulp-util");
+
+gulp.task("clean", () => del("lib/**/*"));
+
+function build() {
+    return gulp.src("src/**/*")
+        .pipe(plumber({
+            errorHandler: (err) => gutil.log(`${err}\n${err.codeFrame}`)
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(cache("src"))
+        .pipe(babel({
+            only: /\.js$/,
+            plugins: [
+                "transform-async-to-generator",
+                "transform-es2015-modules-commonjs"
+            ]
+        }))
+        .pipe(sourcemaps.write("."))
+        .pipe(notify({ message: "attacker: built", onLast: true }))
+        .pipe(gulp.dest("lib"));
+}
+
+gulp.task("build", ["clean"], build);
+
+gulp.task("default", ["build"]);
+
+gulp.task("watch", ["build"], () => {
+    gulp.watch("src/**/*", build);
+});
---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+async function main() {
+    console.log("It works");
+}
+
+main().catch((err) => console.log(err));

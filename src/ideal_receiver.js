9dae8f80b08460cf104fbd636ae8f34b5050270a
stubbornick <38818331+stubbornick@users.noreply.github.com>
2016-10-29 22:12:21 +0200

    * Ideal receiver added for tests

---
+++ src/ideal_receiver.js
@@ -0,0 +1,86 @@
+"use strict"
+
+const net = require("net");
+const fs = require("fs");
+
+const SERVER_HOST = "0.0.0.0";
+const SERVER_PORT = 6666;
+const FLAG_REGEXP = /\w{31}=/;
+const FLAG_LOGFILE= "flags_received.log";
+
+const not_a_flag = "Is not a flag";
+const already_sent = "Already sent";
+const good_answers = ['Accepted', 'Denied: It is your flag!', 'Denied: Too old'];
+
+let receivedFlags = new Map;
+
+class FlagReceiver extends net.Server
+{
+    constructor(port, host){
+        super();
+
+        this.on("connection", this.handleConnection);
+
+        try{
+            fs.unlinkSync(FLAG_LOGFILE);
+        } catch(e){
+            //empty
+        }
+
+        this.listen({
+            host: host,
+            port: port,
+        }, () => {
+            let addr = this.address();
+            console.log(`Start listening on ${addr.address} ${addr.port}`);
+        });
+    }
+
+    handleConnection(socket){
+        let address = socket.localAddress;
+
+        console.log(`[${address}] connected`);
+
+        socket.on("close", () => {
+            console.log(`[${address}] disconnected`);
+        });
+
+        let buffer = '';
+        socket.on("data", (data) => {
+            for (let char of data.toString()) {
+                if (char === '\n'){
+                    this.processLine(buffer, socket);
+                    buffer = '';
+                } else {
+                    buffer += char;
+                }
+            };
+        });
+    }
+
+    processLine(line, socket){
+        if (line.length === 0){
+            console.log(`[${socket.localAddress}] Goodbye!`);
+            socket.write("Goodbye!\n");
+            socket.end();
+        } else if (FLAG_REGEXP.test(line)) {
+            if (!receivedFlags.has(line)){
+                let answer = good_answers[Math.floor(Math.random() * good_answers.length)];
+                this.answerOnInput(line, answer, socket);
+                receivedFlags.set(line, answer);
+                fs.appendFileSync(FLAG_LOGFILE, line);
+            } else {
+                this.answerOnInput(line, already_sent, socket);
+            }
+        } else {
+            this.answerOnInput(line, not_a_flag, socket);
+        }
+    };
+
+    answerOnInput(input, answer, socket){
+        socket.write(answer+'\n');
+        console.log(`[${socket.localAddress}] ${input} - ${answer}`);
+    }
+}
+
+new FlagReceiver(SERVER_PORT, SERVER_HOST);
